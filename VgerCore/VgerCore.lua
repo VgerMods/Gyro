4787a563313b81350cc9b726b1f3b825de417595
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2026-01-01 08:05:30 -0800

    Happy new year 2026

--- VgerCore/VgerCore.lua
+++ VgerCore/VgerCore.lua
@@ -1,9 +1,9 @@
 ﻿-- VgerCore  by Vger-Azjol-Nerub
 -- www.vgermods.com
--- © 2006-2025 Travis Spomer.  This mod is released under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 license.
+-- © 2006-2026 Travis Spomer.  This mod is released under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 license.
 -- 
--- Version 1.0.19 -- IsWarWithin
-local VgerCoreThisVersion = 1.19
+-- Version 1.0.21 -- IsMidnight
+local VgerCoreThisVersion = 1.21
 -- 
 -- VgerCore contains functionality that is shared by Vger's mods.
 -- It can be used as a standalone add-on, or embedded within other mods.
@@ -24,20 +24,25 @@ VgerCore.IsClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
 VgerCore.IsBurningCrusade = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC and LE_EXPANSION_LEVEL_CURRENT == LE_EXPANSION_BURNING_CRUSADE) -- includes pre-patch
 VgerCore.IsWrath = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC and LE_EXPANSION_WRATH_OF_THE_LICH_KING and LE_EXPANSION_LEVEL_CURRENT >= LE_EXPANSION_WRATH_OF_THE_LICH_KING) or (WOW_PROJECT_WRATH_CLASSIC and WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC) -- includes pre-patch
 VgerCore.IsCataclysm = (WOW_PROJECT_CATACLYSM_CLASSIC and WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC) -- includes pre-patch
+VgerCore.IsMists = (WOW_PROJECT_MISTS_CLASSIC and WOW_PROJECT_ID == WOW_PROJECT_MISTS_CLASSIC) -- includes pre-patch
+VgerCore.IsDraenor = false
+VgerCore.IsLegion = false
+VgerCore.IsBattle = false
 VgerCore.IsMainline = BuildNumber >= 90000
-VgerCore.IsWarWithin = VgerCore.IsMainline and BuildNumber >= 110000
-VgerCore.IsDragonflight = VgerCore.IsMainline and BuildNumber >= 100000
 VgerCore.IsShadowlands = VgerCore.IsMainline and BuildNumber >= 90000
+VgerCore.IsDragonflight = VgerCore.IsMainline and BuildNumber >= 100000
+VgerCore.IsWarWithin = VgerCore.IsMainline and BuildNumber >= 110000
+VgerCore.IsMidnight = VgerCore.IsMainline and BuildNumber >= 120000
 
-VgerCore.DeathKnightsExist = VgerCore.IsWrath or VgerCore.IsCataclysm or VgerCore.IsMainline
-VgerCore.MonksExist = VgerCore.IsMainline
-VgerCore.DemonHuntersExist = VgerCore.IsMainline
+VgerCore.DeathKnightsExist = VgerCore.IsWrath or VgerCore.IsCataclysm or VgerCore.IsMists or VgerCore.IsMainline
+VgerCore.MonksExist = VgerCore.IsMists or VgerCore.IsDraenor or VgerCore.IsLegion or VgerCore.IsBattle or VgerCore.IsMainline
+VgerCore.DemonHuntersExist = VgerCore.IsLegion or VgerCore.IsBattle or VgerCore.IsMainline
 VgerCore.EvokersExist = VgerCore.IsDragonflight
-VgerCore.SpecsExist = VgerCore.IsMainline
+VgerCore.SpecsExist = VgerCore.IsMists or VgerCore.IsLegion or VgerCore.IsBattle or VgerCore.IsMainline
 VgerCore.RangedSlotExists = VgerCore.IsClassic or VgerCore.IsBurningCrusade or VgerCore.IsWrath or VgerCore.IsCataclysm
-VgerCore.ArtifactsExist = VgerCore.IsMainline
-VgerCore.EquipmentSetsExist = VgerCore.IsWrath or VgerCore.IsCataclysm or VgerCore.IsMainline
-VgerCore.ReforgingExists = VgerCore.IsCataclysm
+VgerCore.ArtifactsExist = VgerCore.IsLegion or VgerCore.IsBattle or VgerCore.IsMainline
+VgerCore.EquipmentSetsExist = VgerCore.IsWrath or VgerCore.IsCataclysm or VgerCore.IsMists or VgerCore.IsDraenor or VgerCore.IsLegion or VgerCore.IsBattle or VgerCore.IsMainline
+VgerCore.ReforgingExists = VgerCore.IsCataclysm or VgerCore.IsMists
 
 -- Common colors
 VgerCore.Color = {}
